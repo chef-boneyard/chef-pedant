992a143b577b6d53e455c765050f179dc6e0d85f
Christopher Maier <cm@opscode.com>
2012-11-26 10:54:05 -0500

    Add validator deletion test

--- spec/api/clients/open_source_complete_endpoint_spec.rb
+++ spec/api/clients/open_source_complete_endpoint_spec.rb
@@ -553,15 +553,6 @@ describe "Open Source Client API endpoint", :platform => :open_source, :clients 
       should_have_proper_deletion_behavior(true)
       should_have_proper_deletion_behavior(false)
 
-      # DON'T DO THIS ON RUBY... IT WILL ACTUALLY SUCCEED
-      context 'deleting the validator', :pending => ruby? do
-        let(:requestor){admin_requestor}
-        let(:client_name){open_source_validator_client_name}
-        it 'is not allowed' do
-          should look_like forbidden_response
-        end
-      end
-
       context 'deleting a non-existent client' do
         let(:requestor) {admin_requestor}
         let(:client_name) {pedant_nonexistent_client_name}
@@ -570,7 +561,15 @@ describe "Open Source Client API endpoint", :platform => :open_source, :clients 
         end
       end
 
-      pending 'cannot delete the last admin client'
+      context 'deleting a validator' do
+        include_context 'with temporary testing client' do
+          let(:client_validator){true}
+        end
+        it 'is allowed' do
+          should look_like delete_client_success_response
+        end
+      end
+
     end
   end # /clients/<client>
 end
