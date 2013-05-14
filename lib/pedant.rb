1ea040834695bf297dda3d0ab1256c03bb5494dc
Douglas Triggs <doug@opscode.com>
2013-05-14 11:13:16 -0600

    Force encoding to UTF-8 to fix OHC encoding errors

--- lib/pedant.rb
+++ lib/pedant.rb
@@ -37,6 +37,8 @@ require 'pedant/rspec/common'
 
 module Pedant
   def self.config
+    Encoding.default_external = Encoding::UTF_8
+    Encoding.default_internal = Encoding::UTF_8
     Config
   end
 
