a87304351f4e943f5b2cedca65314ac51f9cbcb1
Ho-Sheng Hsiao <hosh@opscode.com>
2013-08-28 12:09:07 -0700

    Removed ruby?/erlang? switching from nodes specs

--- spec/api/nodes/open_source_permissions_spec.rb
+++ spec/api/nodes/open_source_permissions_spec.rb
@@ -20,16 +20,6 @@ describe 'Node Endpoint Open Source Permission Checks', :platform => :open_sourc
   include Pedant::RSpec::NodeUtil
   include Pedant::OpenSource::PermissionChecks
 
-  def self.ruby?
-    Pedant::Config.ruby_node_endpoint?
-  end
-
-  # TODO: This can be removed when we're on Erchef OSC
-  if ruby?
-    # Merb returns 404s instead of 405s
-    let(:method_not_allowed_response){resource_not_found_response}
-  end
-
   # Until we rename the requestors
   let(:admin_requestor){admin_user}
 
