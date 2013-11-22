9bc834b7bfeae126123d78aef14af78341301db8
Marc Paradise <marc@opscode.com>
2013-11-22 18:16:55 -0500

    disable globbing in curl to prevent bad parse of ipv6 literal

--- spec/api/cookbooks/read_spec.rb
+++ spec/api/cookbooks/read_spec.rb
@@ -305,7 +305,7 @@ describe "Cookbooks API endpoint", :cookbooks do
         end
 
         it "curl" do
-          got = `curl -sk '#{recipe_url}'`
+          got = `curl -gsk '#{recipe_url}'`
           got.should == recipe_content
         end
       end # access to recipe file content
